952da3a9b513332d40311faa365de052d25154a8
Mike Berrio <mikeberrio10@hotmail.com>
2018-07-12 01:20:05 +0000

    mike

--- src/js/component/MeetupsTitle.jsx
+++ src/js/component/MeetupsTitle.jsx
@@ -8,9 +8,9 @@ export class MeetupsTitle extends React.Component{
     
     render(){
         return (
-            <div className="jumbotron jumbotron-fluid" style="padding-right: 210px;">
+            <div className="jumbotron jumbotron-fluid">
                 <div className="container">
-                    <a href="https://placeholder.com"><img src="http://via.placeholder.com/600x350"></img></a>
+                    <a href="https://placeholder.com"><img src="http://via.placeholder.com/600x350"/></a>
                     <h2 className="display-4">Meetup 1</h2>
                     <p className="lead">Location.</p>
                     <p className="lead">Miami, FL</p>
