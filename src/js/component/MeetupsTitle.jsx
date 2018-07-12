1613ae38bfa0b6b8308b59cc023d5147b10a8c19
Mike Berrio <mikeberrio10@hotmail.com>
2018-07-12 00:24:09 +0000

    change meetups

--- src/js/component/MeetupsTitle.jsx
+++ src/js/component/MeetupsTitle.jsx
@@ -8,7 +8,7 @@ export class MeetupsTitle extends React.Component{
     
     render(){
         return (
-            <div className="jumbotron jumbotron-fluid" style="padding-right: 210px;">
+            <div class="jumbotron jumbotron-fluid" style="padding-right: 210px;">
                 <div class="container">
                     <a href="https://placeholder.com"><img src="http://via.placeholder.com/600x350"></img></a>
                     <h2 className="display-4">Meetup 1</h2>
