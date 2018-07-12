3deb4118167fdf3a2732d22fdaae934c520bb523
Edgar Brignoni <edgarbrignoni@icloud.com>
2018-07-12 01:04:13 +0000

    edgar

--- src/js/component/MeetupsTitle.jsx
+++ src/js/component/MeetupsTitle.jsx
@@ -8,7 +8,7 @@ export class MeetupsTitle extends React.Component{
     
     render(){
         return (
-            <div className="jumbotron jumbotron-fluid" style="padding-right: 210px;">
+            <div className="jumbotron jumbotron-fluid">
                 <div className="container">
                     <a href="https://placeholder.com"><img src="http://via.placeholder.com/600x350"></img></a>
                     <h2 className="display-4">Meetup 1</h2>
