b3217b604edd77ffd234e026ec972f3214392bb3
Edgar Brignoni <edgarbrignoni@icloud.com>
2018-07-12 01:01:24 +0000

    edgar

--- src/js/component/NavBar.jsx
+++ src/js/component/NavBar.jsx
@@ -3,19 +3,13 @@ import { Link } from "react-router-dom";
 
 function NavBar(){
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">React Router Mini-Project</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                <div className="text">Menu</div>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div className="navbar-nav">
-                    <Link to={"/"} className="nav-item nav-link" >Home <span className="sr-only">(current)</span></Link>
-                    <Link to={"/events"} className="nav-item nav-link" >Events</Link>
-                    <Link to={"/meetups"} className="nav-item nav-link" >Meetups</Link>
-                </div>
-            </div>
+        <nav className="navbar navbar-light bg-light">
+            <Link to={"/"} className="nav-item nav-link" >
+                <img src="http://via.placeholder.com/200x50" />
+            </Link>
+            <form className="form-inline">
+                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
         </nav>
     );
 }
