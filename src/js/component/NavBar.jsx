eb5b2420786b3e810d1d3e13fd5f795c581ee067
Edgar Brignoni <edgarbrignoni@icloud.com>
2018-07-13 16:13:49 +0000

    edgar

--- src/js/component/NavBar.jsx
+++ src/js/component/NavBar.jsx
@@ -8,11 +8,7 @@ function NavBar(){
                 <img className="img-fluid" src="https://www.4geeksacademy.co/wp-content/themes/the-fastest/assets/img/logo-black.png" />
             </Link>
             <form className="form-inline">
-<<<<<<< HEAD
                 <button className="btn btn-primary my-2 my-sm-0" type="submit">Login</button>
-=======
-                <button className="btn btn-primary my-2 my-sm-0 " type="submit">Login</button>
->>>>>>> 30ef9551b466c5c847a376b7200d52b50d4e0958
             </form>
         </nav>
     );
